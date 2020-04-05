2b6b735faad2dda7ec59a3ad30e7688721717306
agentdata <brantly.w@gmail.com>
2020-04-05 15:27:29 -0600

    added mysql recipe instructions

--- BlabberApp/BlabberApp.DataStore/ApplicationContext.cs
+++ BlabberApp/BlabberApp.DataStore/ApplicationContext.cs
@@ -14,5 +14,13 @@ namespace BlabberApp.DataStore
             new BlabMap(modelBuilder.Entity<Blab>());
             new UserMap(modelBuilder.Entity<User>());
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Warning To protect potentially sensitive information in your connection string,
+            // you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263
+            // for guidance on storing connection strings.
+            optionsBuilder.UseMySQL("server=142.93.114;database=agentdata;user=agentdata;password=letmein");
+        }
     }
 }
