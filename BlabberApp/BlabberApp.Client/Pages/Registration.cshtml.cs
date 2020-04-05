b0f4861e985eba5975f7b096b4b7c97cfcd5457b
agentdata <brantly.w@gmail.com>
2020-04-05 15:22:04 -0600

    modified pages

--- BlabberApp/BlabberApp.Client/Pages/Registration.cshtml.cs
+++ BlabberApp/BlabberApp.Client/Pages/Registration.cshtml.cs
@@ -1,25 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace BlabberApp.Client.Pages
+namespace BlabberApp.Client
 {
     public class RegistrationModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public RegistrationModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
+        }
 
+        public void OnPost()
+        {
+            
         }
     }
 }
